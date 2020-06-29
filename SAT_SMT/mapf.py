e7c3ee7c850e4daff82e04c941a3e5b253c975a1
alessandro <alelombo55@gmail.com>
2020-06-29 16:33:27 +0200

    Improve constraints 3 and 4. Use Solver().

--- SAT_SMT/mapf.py
+++ SAT_SMT/mapf.py
@@ -8,7 +8,7 @@ from z3 import *
 # x = src node
 # y = dst node
 # a directed arc exists between x and y
-arc_ = Function('arc', IntSort(), IntSort(), IntSort())
+arc_ = Function('arc', IntSort(), IntSort(), BoolSort())
 
 # orig(a)
 # a = agent
@@ -24,14 +24,14 @@ dest_ = Function('dest', IntSort(), IntSort())
 # x = vertex
 # a = agent
 # t = time step
-at_ = Function('at', IntSort(), IntSort(), IntSort(), IntSort())
+at_ = Function('at', IntSort(), IntSort(), IntSort(), BoolSort())
 
 # pass(x, y, a, t)
 # x = source vertex
 # y = destination vertex
 # a = agent
 # t = time step
-pass_ = Function('pass', IntSort(), IntSort(), IntSort(), IntSort(), IntSort())
+pass_ = Function('pass', IntSort(), IntSort(), IntSort(), IntSort(), BoolSort())
 
 # ======================================================================================================================
 # Set of variables
@@ -39,6 +39,7 @@ pass_ = Function('pass', IntSort(), IntSort(), IntSort(), IntSort(), IntSort())
 
 A_size = 2
 V_size = 2
+E_size = 4
 T_size = 1
 
 # Vertexes
@@ -47,6 +48,12 @@ T_size = 1
 V = Array('V', IntSort(), IntSort())
 for i in range(V_size):
     V = Store(V, i, i)
+
+# Edges
+# ----------------------------------------------------------------------------------------------------------------------
+
+# Edges = [(0, 1), (0, 0), (1, 1), (1, 0)]
+E = [arc_(V[0], V[1]), arc_(V[0], V[0]), arc_(V[1], V[0]), arc_(V[1], V[1])]
 
 # Time steps
 # ----------------------------------------------------------------------------------------------------------------------
@@ -67,28 +74,45 @@ for i in range(A_size):
 
 # Sum over vertices (SV)
 
-SV_temp = [[[at_(vertex, agent, time) for vertex in range(V_size)] for time in range(T_size)] for agent in range(A_size)]
-# FIXED: Swapped agent and time
-SV = [[sum(SV_temp[agent][time]) for time in range(T_size)] for agent in range(A_size)]
+SV_temp = [[[If(at_(V[vertex], A[agent], T[time]), 1, 0)
+             for vertex in range(V_size)]
+            for time in range(T_size)]
+           for agent in range(A_size)]
+
+SV = [[sum(SV_temp[agent][time])
+       for time in range(T_size)]
+      for agent in range(A_size)]
+
 SV = [element for sublist in SV for element in sublist]
 
+"""
 SVZ3 = Array('SVZ3', IntSort(), IntSort())
 index = 0
 for i in SV:
     SVZ3 = Store(SVZ3, index, i)
     index += 1
+"""
 
 # Sum over agents (SA)
 
-SA_temp = [[[at_(vertex, agent, time) for agent in range(A_size)] for time in range(T_size)] for vertex in range(V_size)]
-SA = [[sum(SV_temp[vertex][time]) for time in range(T_size)] for vertex in range(V_size)]
+SA_temp = [[[If(at_(V[vertex], A[agent], T[time]), 1, 0)
+             for agent in range(A_size)]
+            for time in range(T_size)]
+           for vertex in range(V_size)]
+
+SA = [[sum(SA_temp[vertex][time])
+       for time in range(T_size)]
+      for vertex in range(V_size)]
+
 SA = [element for sublist in SA for element in sublist]
 
+"""
 SAZ3 = Array('SAZ3', IntSort(), IntSort())
 index = 0
 for i in SA:
     SAZ3 = Store(SAZ3, index, i)
     index += 1
+"""
 
 # ======================================================================================================================
 # Constraints
@@ -101,59 +125,64 @@ t = Int('t')
 
 s = Solver()
 
-solve(
+# Each agent occupies at most one node (3)
+s.add([sv <= 1 for sv in SV])
+# Every node is occupied by at most one agent (4)
+s.add([sa <= 1 for sa in SA])
+
+s.add(
     # Graph
-    arc_(V[0], V[1]) == 1,
-    arc_(V[0], V[0]) == 1,
-    arc_(V[1], V[0]) == 1,
-    arc_(V[1], V[1]) == 1,
+    E +
 
-    # Agent 0
-    orig_(A[0]) == V[0],
-    dest_(A[0]) == V[0],
-    at_(orig_(A[0]), A[0], T[0]) == 1,
-    # TODO: Check if necessary
-    # at_(V[1], A[0], T[0]) == 0,
+    [
+        # Agent 0
+        orig_(A[0]) == V[0],
+        dest_(A[0]) == V[0],
+        at_(orig_(A[0]), A[0], T[0]),
+        # TODO: Check if necessary
+        # at_(V[1], A[0], T[0]) == False,
 
-    # Agent 1
-    orig_(A[1]) == V[1],
-    dest_(A[1]) == V[1],
-    at_(orig_(A[1]), A[1], T[0]) == 1,
-    # TODO: Check if necessary
-    # at_(V[0], A[1], T[0]) == 0,
+        # Agent 1
+        orig_(A[1]) == V[1],
+        dest_(A[1]) == V[1],
+        at_(orig_(A[1]), A[1], T[0]),
+        # TODO: Check if necessary
+        # at_(V[0], A[1], T[0]) == False,
 
-    # Start position
-    ForAll([a], Implies(
-        And(a >= 0, a < A_size), at_(orig_(A[a]), A[a], 0) == 1)),
+        # Start position (1)
+        ForAll([a], Implies(
+            And(a >= 0, a < A_size), at_(orig_(A[a]), A[a], 0))),
 
-    # Final position
-    ForAll([a], Implies(
-        And(a >= 0, a < A_size), at_(dest_(A[a]), A[a], T_size - 1) == 1)),
+        # Final position (2)
+        ForAll([a], Implies(
+            And(a >= 0, a < A_size), at_(dest_(A[a]), A[a], T_size - 1))),
 
-    # Each agent occupies at most one node
-    ForAll(x, Implies(And(x >= 0, x < V_size), SVZ3[x] <= 1)),
+        # Each agent occupies at most one node
+        # ForAll(x, Implies(And(x >= 0, x < V_size), SVZ3[x] <= 1)),
 
-    # Every node is occupied by at most one agent
-    ForAll(a, Implies(And(a >= 0, a < A_size), SAZ3[a] <= 1)),
+        # Every node is occupied by at most one agent
+        # ForAll(a, Implies(And(a >= 0, a < A_size), SAZ3[a] <= 1)),
 
-    # If an agent is in a node it needs to leave by one of the outgoing arcs
+        # If an agent is in a node it needs to leave by one of the outgoing arcs (5)
 
-    # TODO
+        # TODO
 
-    # If an agent is using an arc, it needs to arrive at the corresponding node in the next time step
-    ForAll([x, y, a, t], Implies(
-        And(x >= 0, x < V_size, y >= 0, y < V_size, t >= 0, t < T_size - 1, a >= 0, a < A_size, arc_(V[x], V[y]) == 1),
-        Implies(
-            pass_(V[x], V[y], A[a], T[t]) == 1,
-            at_(V[y], A[a], T[t + 1]) == 1
-        )
-    ))
+        # If an agent is using an arc, it needs to arrive at the corresponding node in the next time step (6)
+        ForAll([x, y, a, t], Implies(
+            And(x >= 0, x < V_size, y >= 0, y < V_size, t >= 0, t < T_size - 1, a >= 0, a < A_size, arc_(V[x], V[y])),
+            Implies(
+                pass_(V[x], V[y], A[a], T[t]),
+                at_(V[y], A[a], T[t + 1])
+            )
+        ))
 
-    # Two agents can't occupy two opposite arcs at the same time (no-swap constraint)
+        # Two agents can't occupy two opposite arcs at the same time (no-swap constraint) (7)
 
-    # TODO
+        # TODO
+    ]
 )
 
 print(s.check())
+print(s.model())
 
 # ----------------------------------------------------------------------------------------------------------------------
